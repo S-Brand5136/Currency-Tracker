85f5412d7822752e034eb31673c621c44a37f37f
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2022-04-03 01:00:24 -0600

    Create useEffect and useState to set marketData on load

--- screens/HomeTab.tsx
+++ screens/HomeTab.tsx
@@ -1,15 +1,21 @@
+import { useEffect, useState } from "react";
 import { StyleSheet } from "react-native";
 import { Text, View } from "../components/Themed";
 import { RootTabScreenProps } from "../@types/types";
+import TrendingList from "../components/TrendingList";
+import { CurrentTrendData, fetchMarketData } from "../api/coinCap";
 
 export default function HomeTab({ navigation }: RootTabScreenProps<"TabOne">) {
+  const [marketData, setMarketData] = useState<CurrentTrendData[]>([]);
+
+  useEffect(() => {
+    fetchMarketData((data: CurrentTrendData[]) => setMarketData([...data]));
+  }, []);
+
   return (
     <View style={styles.container}>
-      <View
-        style={styles.separator}
-        lightColor='#eee'
-        darkColor='rgba(255,255,255,0.1)'
-      />
+      <Text style={styles.title}>Trending Coins</Text>
+      <TrendingList data={marketData} />
     </View>
   );
 }
@@ -23,10 +29,8 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 20,
     fontWeight: "bold",
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: "80%",
+    textAlign: "left",
+    width: "100%",
+    marginLeft: 25,
   },
 });

b0f56c184a73b97404fd4ce19c5db101f952c14f
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2022-04-03 00:33:53 -0600

    Create CurrentTrendData interface

--- api/coinCap.ts
+++ api/coinCap.ts
@@ -1,9 +1,21 @@
 import axios from "axios";
 
+export interface CurrentTrendData {
+  id: string;
+  symbol: string;
+  rank: string;
+  supply: string;
+  marketCapUsd: string;
+  volumeUsd24Hr: string;
+  priceUsd: string;
+  changePercent24Hr: string;
+  vwrap24Hr: string;
+}
+
 const baseUrl = "api.coincap.io/v2/";
 
 export const fetchMarketData = async () => {
-  const response = await axios.get<string>(`${baseUrl}assets`);
+  const response = await axios.get<CurrentTrendData[]>(`${baseUrl}assets`);
 
   return response.data;
 };

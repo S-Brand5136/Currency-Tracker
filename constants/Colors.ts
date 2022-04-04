52fd9f82f28abe2192d106852655098c9a4ef3e9
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2022-04-04 16:40:44 -0600

    Update light background color

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -4,7 +4,7 @@ const tintColorDark = "#fff";
 export default {
   light: {
     text: "#000",
-    background: "#fff",
+    background: "#F8F9FA",
     tint: tintColorLight,
     tabIconDefault: "#ccc",
     tabIconSelected: tintColorLight,

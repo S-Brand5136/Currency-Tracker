f88c7814bd76e9aafb9e83230dc1d17400c466cf
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2022-04-08 22:56:52 -0600

    Add StackGroup w/Modal Stack Screen

--- navigation/RootNavigator.tsx
+++ navigation/RootNavigator.tsx
@@ -2,6 +2,7 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { RootStackParamList } from "../@types/types";
 import { BottomTabNavigator } from "./BottomTabNavigator";
 import NotFoundScreen from "../screens/NotFoundScreen";
+import CoinInfoModal from "../screens/CoinInfoModal";
 
 /**
  * A root stack navigator is often used for displaying modals on top of all other content.
@@ -22,6 +23,9 @@ export const RootNavigator = () => {
         component={NotFoundScreen}
         options={{ title: "Oops!" }}
       />
+      <Stack.Group screenOptions={{ presentation: "modal" }}>
+        <Stack.Screen name='Modal' component={CoinInfoModal} />
+      </Stack.Group>
     </Stack.Navigator>
   );
 };

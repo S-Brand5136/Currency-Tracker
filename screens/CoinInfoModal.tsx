9843fdea844cc48ea469d0492c4b68add7e60aa7
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2022-04-09 22:53:55 -0600

    Adjust listItem fonts, and rank icon color

--- screens/CoinInfoModal.tsx
+++ screens/CoinInfoModal.tsx
@@ -2,24 +2,34 @@ import { useEffect } from "react";
 import { RouteProp, useNavigation, useRoute } from "@react-navigation/native";
 import { StyleSheet, Text, View } from "react-native";
 import { RootStackParamList } from "../@types/types";
+import useAxios from "../hooks/useAxios";
+import HistoryChart from "../components/HistoryChart";
 
 type Props = {};
 
 const CoinInfoModal = (props: Props) => {
-  const navigation = useNavigation();
   const {
     params: { title, id },
   } = useRoute<RouteProp<RootStackParamList, "Modal">>();
+  const navigation = useNavigation();
 
   useEffect(() => {
     navigation.setOptions({
       title,
       id,
     });
-  }, [navigation]);
+  }, []);
+
+  const { response, loading, error, sendData } = useAxios(
+    {
+      method: "GET",
+      url: `/assets/${id}/history?interval=d1`,
+    },
+    "https://api.coincap.io/v2"
+  );
 
   return (
-    <View>
+    <View style={styles.layout}>
       <Text>CoinInfoModal</Text>
     </View>
   );
@@ -27,4 +37,6 @@ const CoinInfoModal = (props: Props) => {
 
 export default CoinInfoModal;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  layout: {},
+});

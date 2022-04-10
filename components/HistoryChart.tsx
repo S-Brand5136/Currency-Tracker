53bc5a568486981ce6380dd43abc0ac45ad1325b
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2022-04-09 22:09:22 -0600

    Create base HistoryChart component

---
+++ components/HistoryChart.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { LineChart } from "react-native-chart-kit";
+
+type Props = {};
+
+const HistoryChart = (props: Props) => {
+  return (
+    <View>
+      <Text>HistoryChart</Text>
+    </View>
+  );
+};
+
+export default HistoryChart;
+
+const styles = StyleSheet.create({});

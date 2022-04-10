9b67b713b794ac396cdebea41fbc7a4a9da412d4
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2022-04-09 21:28:04 -0600

    Create useAxios hook

---
+++ hooks/useAxios.ts
@@ -0,0 +1,35 @@
+import { useState, useEffect } from "react";
+import axios, { AxiosError, AxiosRequestConfig, AxiosResponse } from "axios";
+
+const useAxios = (axiosParams: AxiosRequestConfig, baseURL: string) => {
+  axios.defaults.baseURL = baseURL;
+
+  const [response, setResponse] = useState<AxiosResponse>();
+  const [error, setError] = useState<AxiosError>();
+  const [loading, setLoading] = useState(true);
+
+  const fetchData = async (params: AxiosRequestConfig) => {
+    try {
+      const result = await axios.request(params);
+      setResponse(result);
+    } catch (err: AxiosError | any) {
+      setError(err);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const sendData = () => {
+    fetchData(axiosParams);
+  };
+
+  useEffect(() => {
+    if (axiosParams.method === "GET" || axiosParams.method === "get") {
+      fetchData(axiosParams);
+    }
+  }, []);
+
+  return { response, error, loading, sendData };
+};
+
+export default useAxios;

b5f163988c219a7fd782983542647a6900cac959
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2022-04-10 23:03:41 -0600

    Fix styling

--- components/ModalHeader.tsx
+++ components/ModalHeader.tsx
@@ -8,22 +8,9 @@ type Props = {
 };
 const ModalHeader = (props: Props) => {
   return (
-    <View
-      style={{
-        flexDirection: "row",
-        alignItems: "center",
-      }}
-    >
-      <Text
-        style={{ fontSize: 22, marginRight: 5, fontFamily: "Cabin-Regular" }}
-      >
-        {props.title}
-      </Text>
-      <Text
-        style={{ fontSize: 12, marginTop: 8, opacity: 0.6, marginRight: 15 }}
-      >
-        ({props.symbol})
-      </Text>
+    <View style={styles.layout}>
+      <Text style={styles.title}>{props.title}</Text>
+      <Text style={styles.symbol}>({props.symbol})</Text>
       <TouchableOpacity>
         <AntDesign size={24} name='staro' />
       </TouchableOpacity>
@@ -33,4 +20,21 @@ const ModalHeader = (props: Props) => {
 
 export default ModalHeader;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  layout: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 22,
+    marginRight: 5,
+    fontFamily: "Cabin-Regular",
+  },
+  symbol: {
+    fontSize: 12,
+    fontFamily: "Cabin-Regular",
+    marginTop: 8,
+    opacity: 0.6,
+    marginRight: 15,
+  },
+});

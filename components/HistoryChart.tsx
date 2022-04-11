0870fc70b2b828dbf5a19f5ce5aa41413721c33a
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2022-04-11 09:29:10 -0600

    Create base Historychart component

--- components/HistoryChart.tsx
+++ components/HistoryChart.tsx
@@ -1,17 +1,54 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Dimensions } from "react-native";
+import { StyleSheet, View } from "react-native";
 import { LineChart } from "react-native-chart-kit";
 
 type Props = {};
 
+// Dummy data
+const data = {
+  labels: ["January", "February", "March", "April", "May", "June"],
+  datasets: [
+    {
+      data: [20, 45, 28, 80, 99, 43],
+      color: (opacity = 1) => `rgba(0, 99, 245, ${opacity})`,
+    },
+  ],
+};
+
+const chartConfig = {
+  backgroundGradientFrom: "#fff",
+  backgroundGradientTo: "#fff",
+  color: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
+  barPercentage: 0.5,
+};
+
 const HistoryChart = (props: Props) => {
+  const screenWidth = Dimensions.get("window").width;
+
   return (
-    <View>
-      <Text>HistoryChart</Text>
+    <View style={styles.layout}>
+      <LineChart
+        data={data}
+        width={screenWidth + 50}
+        height={220}
+        chartConfig={chartConfig}
+        withHorizontalLines={false}
+        withVerticalLines={false}
+        withShadow={false}
+        withDots={false}
+        withHorizontalLabels={false}
+      />
     </View>
   );
 };
 
 export default HistoryChart;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  layout: {
+    flexDirection: "row",
+    justifyContent: "center",
+    marginTop: 18,
+  },
+});

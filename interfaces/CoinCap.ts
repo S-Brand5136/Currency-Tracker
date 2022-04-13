5508c4f3e706eebe0a7b9ef80dc9942f58e6bad1
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2022-04-12 22:00:38 -0600

    Remove unused variables from interface declaration

--- interfaces/CoinCap.ts
+++ interfaces/CoinCap.ts
@@ -2,12 +2,8 @@ export interface CurrentTrendData {
   id: string;
   symbol: string;
   rank: string;
-  supply: string;
-  marketCapUsd: string;
-  volumeUsd24Hr: string;
   priceUsd: string;
   changePercent24Hr: string;
-  vwrap24Hr: string;
 }
 
 export interface CoinHistory {

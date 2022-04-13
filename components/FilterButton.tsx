7efcf9a43a70b950956ad15105bccac298c8e008
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2022-04-12 21:59:40 -0600

    Add Filter button styling

--- components/FilterButton.tsx
+++ components/FilterButton.tsx
@@ -3,14 +3,25 @@ import React from "react";
 
 type Props = {
   title: string;
+  selected: boolean;
   onPress: Function;
 };
 
 const FilterButton = (props: Props) => {
+  const selectedStyling = props.selected
+    ? {
+        color: "#0063F5",
+        backgroundColor: "#ecf4ff",
+        borderColor: "#0063F5",
+      }
+    : {};
+
   return (
-    <View style={styles.layout}>
+    <View>
       <TouchableOpacity onPress={() => props.onPress()}>
-        <Text style={styles.title}>{props.title}</Text>
+        <Text style={{ ...styles.title, ...selectedStyling }}>
+          {props.title}
+        </Text>
       </TouchableOpacity>
     </View>
   );
@@ -19,6 +30,13 @@ const FilterButton = (props: Props) => {
 export default FilterButton;
 
 const styles = StyleSheet.create({
-  layout: {},
-  title: {},
+  title: {
+    color: "#6C757D",
+    backgroundColor: "#F8F9FA",
+    borderColor: "#DFE2E4",
+    borderWidth: 1,
+    borderRadius: 50,
+    padding: 4,
+    paddingHorizontal: 10,
+  },
 });

ec0ed30fe1dbc85bf686e161d2fc0a7a0f54bef9
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2022-05-18 22:14:17 -0600

    Replace default Light background colour

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -4,7 +4,7 @@ const tintColorDark = "#fff";
 export default {
   light: {
     text: "#000",
-    background: "#F8F9FA",
+    background: "#fff",
     tint: tintColorLight,
     tabIconDefault: "#ccc",
     tabIconSelected: tintColorLight,

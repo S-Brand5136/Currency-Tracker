2e0a1e6eefcd0c02267bfdf7782a0bc1a49ec807
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2022-05-23 14:39:11 -0600

    Add AuthContext, Basic ProfileBanner component

--- screens/ProfileTab.tsx
+++ screens/ProfileTab.tsx
@@ -1,16 +1,41 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { useContext } from "react";
+import { StyleSheet } from "react-native";
+import { AuthContext, AuthContextType } from "../context/AuthContext";
+import { ProfileBanner } from "../components/Banner";
+import { BaseView } from "../components/StyledView";
 
 type Props = {};
 
 const ProfileTab = (props: Props) => {
+  const { user } = useContext(AuthContext) as AuthContextType;
+
+  console.log(user);
+
   return (
-    <View>
-      <Text>ProfileTab</Text>
-    </View>
+    <BaseView
+      alignItems='center'
+      justifyContent='center'
+      lightColor='#F8F9FA'
+      style={styles.container}
+    >
+      <ProfileBanner
+        title={
+          user.username
+            ? `Welcome, ${user.username}. Here you can view all your favourite currencies!`
+            : "Welcome, Login or Register an account to start favourting currencies!"
+        }
+        username={user.username ? user.email : "Get Started!"}
+        color={"#00c254"}
+      />
+    </BaseView>
   );
 };
 
 export default ProfileTab;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: 40,
+    flex: 1,
+  },
+});

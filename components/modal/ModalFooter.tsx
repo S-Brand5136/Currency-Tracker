cb940598ed61010f8a18d3a99c28e8d4105fecb0
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2022-05-31 22:37:38 -0600

    Refactor ModalFooter to use new PrimaryButton

--- components/modal/ModalFooter.tsx
+++ components/modal/ModalFooter.tsx
@@ -1,39 +1,20 @@
-import { View, TouchableOpacity, StyleSheet } from "react-native";
+import { View, StyleSheet } from "react-native";
 import React from "react";
-import { BoldText } from "../StyledText";
-
-type Props = {
-  title: string;
-};
-
-const ButtonText = (props: Props) => {
-  return (
-    <BoldText
-      darkColor='#fff'
-      lightColor='#fff'
-      fontSize={18}
-      letterSpacing={0.75}
-    >
-      {props.title}
-    </BoldText>
-  );
-};
+import PrimaryButton from "../buttons/PrimaryButton";
 
 const ModalFooter = () => {
   return (
     <View style={styles.layout}>
-      <TouchableOpacity
-        style={styles.buySellButton}
-        onPress={() => console.log("Buy")}
-      >
-        <ButtonText title='BUY' />
-      </TouchableOpacity>
-      <TouchableOpacity
-        style={styles.buySellButton}
-        onPress={() => console.log("Sell")}
-      >
-        <ButtonText title='SELL' />
-      </TouchableOpacity>
+      <PrimaryButton
+        themedStyle={{ width: "47%", height: "75%" }}
+        title={"Buy"}
+        pressed={() => console.log("Buy Pressed")}
+      />
+      <PrimaryButton
+        themedStyle={{ width: "47%", height: "75%" }}
+        title={"Sell"}
+        pressed={() => console.log("Sell Pressed")}
+      />
     </View>
   );
 };
@@ -53,15 +34,6 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     justifyContent: "space-between",
     alignContent: "space-around",
-  },
-  buySellButton: {
-    backgroundColor: "#0063F5",
-    borderRadius: 5,
-    width: "47%",
-    height: "75%",
-    marginBottom: 15,
-    justifyContent: "center",
-    alignItems: "center",
   },
 });
 
